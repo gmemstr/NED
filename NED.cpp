551665dc9dc4e4972b52a7d41e4cfcb05d2e06ad
gmemstr <bladesimmer@gmail.com>
2013-03-31 17:32:28 -0700

    Create NED.cpp

---
+++ NED.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "NED" << endl;
+   cout << "Type and press [ENTER] when done." << endl ;
+   cout << " " << endl;
+   string a ;
+   cin >> a;
+
+    return 0;
+}
