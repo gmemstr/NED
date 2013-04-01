155fa1259140e1a153a8eac2a5dd72953d54a25a
gmemstr <bladesimmer@gmail.com>
2013-03-31 17:33:31 -0700

    Update NED.cpp

--- NED.cpp
+++ NED.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main()
 {
+   //Super simple, hope to expand
    cout << "NED" << endl;
    cout << "Type and press [ENTER] when done." << endl ;
    cout << " " << endl;
+   //Used to be a CHAR variable :P
    string a ;
    cin >> a;
-
+   //VIEW README
     return 0;
 }
